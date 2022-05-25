d441f389720bbcfc21e9a8d5dc924755dd720141
myefomit <maslovtimofey@gmail.com>
2022-05-25 14:55:45 +0500

    add articles post spec

--- spec/requests/article_spec.rb
+++ spec/requests/article_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe '/articles', type: :request do
     before(:each) { get path }
     subject { response }
 
-    it { is_expected.to have_http_status(200) }
+    it { is_expected.to have_http_status(:success) }
 
     context 'when no entities' do
       it 'returns empty array' do
@@ -25,6 +25,16 @@ RSpec.describe '/articles', type: :request do
   end
 
   describe 'POST' do
-    before(:each) { post '/articles', params }
+    let(:params) { { params: { article: article_params } } }
+    before(:each) do
+      post '/articles', params
+    end
+    subject { response }
+
+    context 'when correct params' do
+      let(:article_params) { attributes_for(:article) }
+
+      it { is_expected.to have_http_status(:created) }
+    end
   end
 end
