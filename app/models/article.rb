251de83196acc6179af303f57037e7d055dde3ef
myefomit <maslovtimofey@gmail.com>
2022-05-25 23:40:03 +0500

    add ntm through article_tags

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ApplicationRecord
-  has_many :tags
+  has_many :article_tags
+  has_many :tags, through: :article_tags
 
   validates :url, uniqueness: true
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_23_131925) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_25_183211) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "article_tags", force: :cascade do |t|
+    t.bigint "article_id", null: false
+    t.bigint "tag_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_article_tags_on_article_id"
+    t.index ["tag_id"], name: "index_article_tags_on_tag_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -32,4 +41,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_23_131925) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "article_tags", "articles"
+  add_foreign_key "article_tags", "tags"
 end
